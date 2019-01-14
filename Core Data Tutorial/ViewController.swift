f0c5d7b0421c3acfa24589b0765b32fae15ca399
Pandu <pandurang.yachwad@gmail.com>
2019-01-14 00:07:06 -0600

    Added wireframe for the tableview and data addition

--- Core Data Tutorial/ViewController.swift
+++ Core Data Tutorial/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var names = [String]()
 
+    @IBOutlet var listTableView: UITableView!
+    @IBAction func addNewItem(_ sender: Any) {
+        let alert = UIAlertController(title: "New Name", message: "Add new a name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            self.names.append(nameToSave)
+            self.listTableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
 }
 
